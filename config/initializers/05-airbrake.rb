301ba7650ef5f36ffbd19615a56619d5d1a297da
Stefan Sundin <git@stefansundin.com>
2015-12-21 19:31:03 -0800

    Update Airbrake config

--- config/initializers/05-airbrake.rb
+++ config/initializers/05-airbrake.rb
@@ -1,13 +1,10 @@
-if ENV["AIRBRAKE_KEY"]
+if ENV["AIRBRAKE_API_KEY"]
   Airbrake.configure do |config|
     config.host = ENV["AIRBRAKE_HOST"]
-    config.api_key = ENV["AIRBRAKE_KEY"]
-    config.secure = true
-
-    # Uncomment this to get errors in development environment:
-    config.development_environments = []
+    config.project_id = ENV["AIRBRAKE_PROJECT_ID"]
+    config.project_key = ENV["AIRBRAKE_API_KEY"]
   end
 
-  use Airbrake::Sinatra
+  use Airbrake::Rack::Middleware
   enable :raise_errors
 end
