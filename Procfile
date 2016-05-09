165f5d22689f3dd41a1f35501694ce1b57bb031e
Stefan Sundin <git@stefansundin.com>
2016-05-08 19:10:02 -0700

    Switch to puma.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bundle exec unicorn -p $PORT -c config/unicorn.rb -N
+web: bundle exec puma -p $PORT -C config/puma.rb
--- config/application.rb
+++ config/application.rb
@@ -26,3 +26,6 @@ end
 app_path = File.expand_path("../..", __FILE__)
 Dir["#{app_path}/config/initializers/*.rb"].each { |f| require f }
 Dir["#{app_path}/app/**/*.rb"].each { |f| require f }
+
+# puma autoload warnings
+require 'tilt/erubis'
