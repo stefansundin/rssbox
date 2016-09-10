9c10be125e0098603634e91d05f23782ddeb9ee2
Stefan Sundin <git@stefansundin.com>
2016-09-09 19:08:49 -0700

    Include twitter video clips.

---
+++ config/initializers/05-bool.rb
@@ -0,0 +1,19 @@
+class FalseClass
+  def <=>(other)
+    if other == true
+      return -1
+    else
+      return 0
+    end
+  end
+end
+
+class TrueClass
+  def <=>(other)
+    if other == false
+      return 1
+    else
+      return 0
+    end
+  end
+end
