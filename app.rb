c5a39bfeddfeb50513acf8081b4f75e9b9cc50f1
Stefan Sundin <git@stefansundin.com>
2016-09-15 21:54:33 -0700

    Handle twitter.com/@username.

--- app.rb
+++ app.rb
@@ -52,7 +52,7 @@ get "/twitter" do
     return "Unsupported url. Sorry."
   elsif /twitter\.com\/hashtag\// =~ params[:q]
     return "This app does not support hashtags. Sorry."
-  elsif /twitter\.com\/(?:#!\/)?(?<user>[^\/?#]+)/ =~ params[:q]
+  elsif /twitter\.com\/(?:#!\/|@)?(?<user>[^\/?#]+)/ =~ params[:q]
     # https://twitter.com/#!/infected
     # https://twitter.com/infected
   else
