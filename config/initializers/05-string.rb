5708ec30ecd8f76985b04e8719bbd0f97beb4438
Stefan Sundin <git@stefansundin.com>
2017-01-07 22:29:02 -0800

    Put tweet videos above pictures. Embed the videos with proper width and height.

--- config/initializers/05-string.rb
+++ config/initializers/05-string.rb
@@ -230,7 +230,10 @@ class String
     elsif %r{^https?://[a-z0-9\-._~:/?#\[\]@!$&'()*+,;=]+\.(?:gif|jpg|png)(?::large)?}i =~ self
       "<img src='#{self}'>"
     elsif %r{^https?://[a-z0-9\-._~:/?#\[\]@!$&'()*+,;=]+\.mp4}i =~ self
-      "<iframe width='640' height='538' src='#{self}' frameborder='0' scrolling='no' allowfullscreen></iframe>"
+      query = CGI.parse(URI.parse(self).query)
+      width = query["w"][0] || "640"
+      height = query["h"][0] || "538"
+      "<iframe width='#{width}' height='#{height}' src='#{self}' frameborder='0' scrolling='no' allowfullscreen></iframe>"
     elsif %r{^https?://amp\.twimg\.com/v/.+}i =~ self
       "<iframe width='640' height='600' src='#{self}' frameborder='0' scrolling='no' allowfullscreen></iframe>"
     end
