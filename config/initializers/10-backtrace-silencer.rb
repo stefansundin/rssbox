c3b89137c6e146dc1f540c4d86c48c143e161330
Stefan Sundin <git@stefansundin.com>
2017-11-25 17:11:56 -0800

    Fix backtrace silencer for Heroku.

--- config/initializers/10-backtrace-silencer.rb
+++ config/initializers/10-backtrace-silencer.rb
@@ -8,7 +8,7 @@ configure :deployment do
     def backtrace
       # If this function is called from Sinatra, then we want to only print a partial backtrace in order to cut down on log filesize
       if caller_locations(1,1)[0].path.end_with?("/lib/sinatra/base.rb")
-        return full_backtrace.select { |l| !l["/lib/ruby/gems/"] }
+        return full_backtrace.select { |l| !l.start_with?("/app/vendor/bundle/ruby/") }
       end
       return full_backtrace
     end
