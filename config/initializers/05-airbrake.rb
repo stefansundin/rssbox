6d4211d9747014e14dc1e3b8b3347a9fc494c3c8
Stefan Sundin <git@stefansundin.com>
2018-03-29 21:05:32 -0700

    Airbrake fix.

--- config/initializers/05-airbrake.rb
+++ config/initializers/05-airbrake.rb
@@ -3,7 +3,7 @@
 if ENV["AIRBRAKE_API_KEY"]
   require "airbrake"
   Airbrake.configure do |config|
-    config.host = ENV["AIRBRAKE_HOST"]
+    config.host = ENV["AIRBRAKE_HOST"] if ENV["AIRBRAKE_HOST"]
     config.project_id = ENV["AIRBRAKE_PROJECT_ID"]
     config.project_key = ENV["AIRBRAKE_API_KEY"]
   end
