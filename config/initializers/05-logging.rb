a9190326ad189ed58e09152299df273e4ad32418
Stefan Sundin <git@stefansundin.com>
2018-03-31 16:57:57 -0700

    Update logger format.

--- config/initializers/05-logging.rb
+++ config/initializers/05-logging.rb
@@ -3,5 +3,8 @@
 # disable Sinatra's logger
 disable :logging
 
-# enable combined log format
-use Clogger, format: :Combined, logger: $stdout, reentrant: true
+# https://bogomips.org/clogger/
+# this format is similar to "Combined", but without $time_local and $http_user_agent (and using $ip instead of $remote_addr)
+# removing time and user-agent saves ~50% on log filesize
+# the purpose of ~ is to allow for easier grepping with -E '^~' (i.e. filtering out exceptions and other crap)
+use Clogger, logger: $stdout, reentrant: true, format: '~ $ip "$request" $status $response_length "$http_referer"'
