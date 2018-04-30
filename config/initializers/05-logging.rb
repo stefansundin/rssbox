070963915af79dfbd742a91e37060a925a3ed322
Stefan Sundin <git@stefansundin.com>
2018-04-30 08:05:23 -0700

    Disable clogger and just rely on Heroku's access logs.

--- config/initializers/05-logging.rb
+++ config/initializers/05-logging.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 # disable Sinatra's logger
-disable :logging
+if defined?(Clogger) || ENV["DISABLE_ACCESS_LOG"]
+  disable :logging
+end
 
 # https://bogomips.org/clogger/
 # this format is similar to "Combined", but without $time_local and $http_user_agent (and using $ip instead of $remote_addr)
 # removing time and user-agent saves ~50% on log filesize
 # the purpose of ~ is to allow for easier grepping with -E '^~' (i.e. filtering out exceptions and other crap)
-use Clogger, logger: $stdout, reentrant: true, format: '~ $ip "$request" $status $response_length "$http_referer"'
+if defined?(Clogger) && !ENV["DISABLE_ACCESS_LOG"]
+  use Clogger, logger: $stdout, reentrant: true, format: '~ $ip "$request" $status $response_length "$http_referer"'
+end
