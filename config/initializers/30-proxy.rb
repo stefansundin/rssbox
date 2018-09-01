0ca3d8a14d7760fb105862ecaa8a3972e3c20ea6
Stefan Sundin <git@stefansundin.com>
2018-08-31 17:47:30 -0700

    X-Forwarded-Url is only needed if a subpath is used, but it would break root_url if it was used for the root path. This fixes that. I even made this simple mistake on the EB app.

--- config/initializers/30-proxy.rb
+++ config/initializers/30-proxy.rb
@@ -17,7 +17,7 @@ class Sinatra::Request
   end
 
   def root_url
-    return base_url if !env["HTTP_X_FORWARDED_URL"]
+    return base_url if !env["HTTP_X_FORWARDED_URL"] || env["HTTP_X_FORWARDED_URL"] == url
     forwarded_path = Addressable::URI.parse(env["HTTP_X_FORWARDED_URL"]).path
     uri = Addressable::URI.parse(url)
     uri.path = forwarded_path[0..(forwarded_path.length-uri.path.length-1)]
