0745f9dbbe0ce44a31cab1ca0f49e7644c9ba0dc
Stefan Sundin <git@stefansundin.com>
2018-10-17 18:27:50 -0700

    Throttle reporting external errors to Airbrake. The free quota is so small these days, and I have run out a lot this year because of random Instagram downtime.

--- config/initializers/05-airbrake.rb
+++ config/initializers/05-airbrake.rb
@@ -14,5 +14,21 @@ if ENV["AIRBRAKE_API_KEY"]
   Airbrake.add_filter do |notice|
     # Bots gonna bot
     notice.ignore! if notice[:errors].any? { |e| e[:type] == "Sinatra::NotFound" } && (/\/wp-(?:admin|includes|content|login)/ =~ notice[:context][:url] || /\/facebook\/\d+\/?$/ =~ notice[:context][:url])
+
+    # Throttle errors from external services. My free plan runs out of quota a lot, often because of Instagram issues.
+    # The first error is reported, but a redis key is set that prevents further errors from the same service to be reported, until the key has expired.
+    # The value in the redis key counts the number of throttled errors, although that information is not persisted anywhere.
+    notice[:errors].each do |e|
+      if Object.const_get(e[:type]) <= HTTPError
+        throttle_key = "airbrake_throttle:#{e[:type]}"
+        if $redis.exists(throttle_key)
+          notice.ignore!
+          $redis.incr(throttle_key)
+          puts "Throttling reporting #{e[:type]} to Airbrake. Throttle counter: #{$redis.get(throttle_key)}."
+        else
+          $redis.setex(throttle_key, ENV["AIRBRAKE_THROTTLE_DURATION"] || 3600, 0)
+        end
+      end
+    end
   end
 end
