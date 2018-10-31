3f706a59152ec5fac02c017305f3a3ad92e804af
Stefan Sundin <git@stefansundin.com>
2018-10-30 22:42:21 -0700

    Cache the periscope session_id.

--- app/periscope.rb
+++ app/periscope.rb
@@ -7,14 +7,23 @@ class Periscope < HTTP
   BASE_URL = "https://api.periscope.tv/api/v2"
   ERROR_CLASS = PeriscopeError
 
+  # The session_id is valid for one hour
+  @@session_id = nil
+
   def self.get_broadcasts(user_id)
+    if @@session_id
+      response = get("/getUserBroadcastsPublic", query: { user_id: user_id, session_id: @@session_id })
+      if response.success?
+        return response
+      end
+    end
     response = get("https://www.periscope.tv/cnn")
     raise(ERROR_CLASS, response) if !response.success?
     doc = Nokogiri::HTML(response.body)
     data = doc.at("div#page-container")["data-store"]
     json = JSON.parse(data)
-    session_id = json["SessionToken"]["public"]["broadcastHistory"]["token"]["session_id"]
-    get("/getUserBroadcastsPublic", query: { user_id: user_id, session_id: session_id })
+    @@session_id = json["SessionToken"]["public"]["broadcastHistory"]["token"]["session_id"]
+    get("/getUserBroadcastsPublic", query: { user_id: user_id, session_id: @@session_id })
   end
 end
 
