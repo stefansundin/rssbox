8c7f7c9052bae8e4545350d0eead65656315b6d8
Stefan Sundin <git@stefansundin.com>
2018-11-20 20:56:22 -0800

    Fix metrics labels.

--- config/initializers/40-prometheus.rb
+++ config/initializers/40-prometheus.rb
@@ -1,7 +1,7 @@
 $prometheus = Prometheus::Client.registry
 
 $metrics = {
-  ratelimit: $prometheus.gauge(:ratelimit, docstring: "Remaining ratelimit for external services.", labels: [:service]),
-  requests: $prometheus.counter(:requests, docstring: "Number of requests made to external services.", labels: [:service, :response_code]),
-  urls: $prometheus.counter(:urls, docstring: "Number of URLs resolved."),
+  ratelimit: $prometheus.gauge(:ratelimit, "Remaining ratelimit for external services."),
+  requests: $prometheus.counter(:requests, "Number of requests made to external services."),
+  urls: $prometheus.counter(:urls, "Number of URLs resolved."),
 }
