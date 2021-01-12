add74b8159e7369c895fdbee8f5d97230e7d8280
Stefan Sundin <git@stefansundin.com>
2021-01-12 14:00:34 -0800

    eb: more deploy fixes

--- .platform/hooks/prebuild/rbenv.sh
+++ .platform/hooks/prebuild/rbenv.sh
@@ -3,3 +3,6 @@
 # Delete the .ruby-version file to avoid rbenv errors when there's a conflicting version.
 # Without this it is impossible to perform even patch version upgrades. It is pretty dumb.
 rm -f .ruby-version
+
+# Work around annoying Errno::ENOENT issue?
+mkdir -p /var/app/staging/vendor/bundle/ruby/2.7.0/cache
