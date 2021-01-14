1ab15995417448c9464345a804d07e7c467bcdee
Stefan Sundin <git@stefansundin.com>
2021-01-13 21:56:42 -0800

    Speedrun: remove "level-subcategories".

--- app/speedrun.rb
+++ app/speedrun.rb
@@ -18,33 +18,19 @@ module App
       return @@cache[type][id] if @@cache[type][id]
       value = $redis.get("speedrun:#{type}:#{id}")
       if value
-        @@cache[type][id] = if type == "level-subcategories"
-          JSON.parse(value)
-        else
-          value
-        end
-        return @@cache[type][id]
+        @@cache[type][id] = value
+        return value
       end
 
       if type == "game"
         response = get("/games/#{id}")
         raise(SpeedrunError, response) if !response.success?
-        redis_value = value = response.json["data"]["names"]["international"]
-      elsif type == "level-subcategories"
-        response = get("/levels/#{id}/variables")
-        raise(SpeedrunError, response) if !response.success?
-        value = response.json["data"].select { |var| var["is-subcategory"] }.to_h do |var|
-          [
-            var["id"],
-            var["values"]["values"].to_h do |id, val|
-              [id, val["label"]]
-            end
-          ]
-        end
-        redis_value = value.to_json
+        value = response.json["data"]["names"]["international"]
+      else
+        raise "unsupported type"
       end
 
-      $redis.set("speedrun:#{type}:#{id}", redis_value)
+      $redis.set("speedrun:#{type}:#{id}", value)
       @@cache[type][id] = value
       return value
     end
