f9b3a1156f8d6ba4107a735bf12443841ec06567
Stefan Sundin <git@stefansundin.com>
2021-01-13 23:21:55 -0800

    Add dark mode.

--- public/js/main.js
+++ public/js/main.js
@@ -327,3 +327,53 @@ $(document).ready(async function() {
     }
   }
 });
+
+
+// Dark mode
+
+document.addEventListener("DOMContentLoaded", () => {
+  const checkbox = document.getElementById("dark-mode");
+  checkbox.addEventListener("click", (e) => {
+    if (e.isTrusted) {
+      // user initiated
+      if (e.shiftKey) {
+        localStorage.removeItem("dark-mode");
+        e.target.checked = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        e.target.indeterminate = true;
+      }
+      else {
+        localStorage.setItem("dark-mode", e.target.checked.toString());
+      }
+    }
+
+    if (e.target.checked) {
+      document.body.classList.add("bg-dark");
+      document.body.classList.add("text-light");
+      $(".modal-content").addClass("bg-dark");
+    }
+    else {
+      document.body.classList.remove("bg-dark");
+      document.body.classList.remove("text-light");
+      $(".modal-content").removeClass("bg-dark");
+    }
+
+    const launcher = document.getElementById("img-launcher");
+    if (launcher) {
+      launcher.src = `img/launcher-${e.target.checked ? "dark":"light"}.png`;
+    }
+  });
+
+  const dark_mode = localStorage.getItem("dark-mode");
+  if (dark_mode == "true" || (dark_mode == undefined && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+    checkbox.click();
+  }
+  checkbox.indeterminate = (dark_mode == undefined);
+});
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
+  const checkbox = document.getElementById("dark-mode");
+  if (checkbox.indeterminate && checkbox.checked != e.matches) {
+    checkbox.click();
+    checkbox.indeterminate = true;
+  }
+});
