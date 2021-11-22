dc6500f9cc906b7a34dfd2f61f73472851ba8360
Stefan Sundin <git@stefansundin.com>
2021-11-21 22:23:48 -0800

    Add end anchor to regex.

--- config/initializers/05-string.rb
+++ config/initializers/05-string.rb
@@ -195,7 +195,7 @@ class String
       "<img src='#{self.https}' referrerpolicy='no-referrer'>"
     elsif %r{^https?://video\.twimg\.com/.+/(?<width>\d+)x(?<height>\d+)/.+\.mp4}i =~ self
       "<video width='#{width}' height='#{height}' controls='controls'><source type='video/mp4' src='#{self}'></video>"
-    elsif %r{^https?://[a-z0-9\-._~:/?#\[\]@!$&'()*+,;=]+\.mp4}i =~ self
+    elsif %r{^https?://[a-z0-9\-._~:/\[\]@!$&'()*+,;=]+\.mp4([\?#]|$)}i =~ self
       uri = URI.parse(self) rescue return
       query = CGI.parse(uri.query || "").merge(CGI.parse(uri.fragment || "")) { |key,oldval,newval| oldval + newval }
       width = query["w"][0] || "640"
