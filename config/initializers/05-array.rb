f126452d1d74c66fa512fc6eb05a8cf79c62079e
Stefan Sundin <git@stefansundin.com>
2022-02-21 22:36:26 -0800

    Move files around so that initializers run before all App classes.

--- config/initializers/05-array.rb
+++ config/initializers/05-array.rb
@@ -10,4 +10,9 @@ class Array
     text = arr.shift
     text.linkify_and_embed(request, arr.join("\n"))
   end
+
+  # sort_fs sorts files in subdirectories lower
+  def sort_fs
+    self.sort.sort { |a,b| a.count("/") <=> b.count("/") }
+  end
 end
--- lib/middleware.rb
+++ lib/middleware.rb
@@ -1,17 +1,15 @@
 # only allow search engine indexing of the main page
 
-module App
-  class XRobotsTag
-    def initialize(app)
-      @app = app
-    end
+class XRobotsTag
+  def initialize(app)
+    @app = app
+  end
 
-    def call(env)
-      status, headers, body = @app.call(env)
-      if env["PATH_INFO"] != "/"
-        headers["X-Robots-Tag"] = "noindex"
-      end
-      return [status, headers, body]
+  def call(env)
+    status, headers, body = @app.call(env)
+    if env["PATH_INFO"] != "/"
+      headers["X-Robots-Tag"] = "noindex"
     end
+    return [status, headers, body]
   end
 end
